7f90aed607d6973a6f728eb9969de0e4762c39ef
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2023-07-21 21:24:11 -0400

    Operate function works

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Create basic functions
 const add = (a, b) => {
   return a + b;
 };
@@ -13,3 +14,21 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
   return a / b;
 };
+
+const operate = (num1, num2, op) => {
+  switch (op) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+  }
+};
+
+// Create our vars for later
+let num1;
+let num2;
+let op;
