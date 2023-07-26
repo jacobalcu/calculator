a3ff40eecb877d822fc6f891929e4c5fe567e990
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2023-07-26 12:40:26 -0400

    Have some logic working with ops

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let operators = document.querySelectorAll(".arith");
+let numbers = document.querySelectorAll(".numbers > button");
+let clear = document.querySelectorAll(".clear");
+
+// screen.textContent = "Hello";
+let current = [];
+
+const clearScreen = () => {
+  let screen = document.querySelector(".screen");
+  screen.textContent = "";
+};
+
+const writeNum = (e) => {
+  let screen = document.querySelector(".screen");
+  screen.textContent += e.target.textContent;
+};
+
 // Create basic functions
 const add = (a, b) => {
   return a + b;
@@ -27,6 +44,28 @@ const operate = (num1, num2, op) => {
       return multiply(num1, num2);
   }
 };
+
+// Still working on it
+operators.forEach((op) => {
+  op.addEventListener("click", (e) => {
+    let screen = document.querySelector(".screen");
+    current.push(screen.textContent);
+    current.push(e.target.textContent);
+    screen.textContent = e.target.textContent;
+  });
+});
+
+clear.forEach((op) => {
+  op.addEventListener("click", clearScreen);
+});
+
+numbers.forEach((num) => {
+  num.addEventListener("click", (e) => writeNum(e));
+});
+
+// operators.forEach(operator => {
+//   operator.addEventListener('click', )
+// })
 
 // Create our vars for later
 let num1;
